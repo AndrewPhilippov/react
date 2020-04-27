9cb2d422c7d731131e28e59178c0c1883a7bce68
Andrew <a@fandb.pro>
2020-04-27 23:13:37 +0300

    Looping through components with given data

--- BRAD/Day_2/index.jsx
+++ BRAD/Day_2/index.jsx
@@ -1,33 +1,40 @@
-function OurApp () {
+const pets = [
+    { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
+    { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
+    { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
+    { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
+    { name: "Paws", species: "dog", age: "6", id: 789789789 }
+]
+
+function OurApp() {
     return (
         <>
             <OurHeader />
             <TimeArea />
             <ul>
-                <Pet name={'Meowsalot'} species={'cat'} age={'5'} />
-                <Pet name={'Barksalot'} species={'dog'} age={'2'} />
-                <Pet name={'Fluffy'} species={'rabbit'} age={'3'} />
+                {pets.map(pet => <Pet name={pet.name} species={pet.species} age={pet.age} key={pet.id} />)}
             </ul>
             <Footer />
         </>
     )
 }
 
-function Pet (props) {
+function Pet(props) {
     return <li>{props.name} is a {props.species} and is {props.age} years old.</li>
 }
 
-function OurHeader () {
-    return <h1 className="special">Our amazing App header</h1>
+function Footer() {
+    return <small>Copyright Footer Text</small>
 }
 
-function TimeArea () {
-    return <p>The current time is {new Date().toLocaleString()}</p>
+function TimeArea() {
+    return <p>The current time is {new Date().toLocaleString()}.</p>
 }
 
-function Footer () {
-    return <small>Copyright footer text</small>
+function OurHeader() {
+    return <h1 className="special">Our Amazing App Header</h1>
 }
-setInterval(function () {
+
+setInterval(function() {
     ReactDOM.render(<OurApp />, document.querySelector("#app"))
 }, 1000)
