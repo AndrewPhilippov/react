18f168f7c69611ae6ba6134cbc03bd9c481d3732
Andrew <a@fandb.pro>
2020-05-19 18:04:37 +0300

    Profile page blocks and functionality added

--- BRAD/COMPLEX-APP/app/Main.js
+++ BRAD/COMPLEX-APP/app/Main.js
@@ -1,96 +1,105 @@
+// Base
 import React, { useState, useReducer, useEffect } from 'react'
 import ReactDOM                                   from 'react-dom'
 import { useImmerReducer }                        from 'use-immer'
 import { BrowserRouter, Switch, Route }           from 'react-router-dom'
-import CreatePost                                 from './components/CreatePost'
-import Header                                     from './components/Header'
-import HomeGuest                                  from './components/HomeGuest'
-import Footer                                     from './components/Footer'
-import About                                      from './components/About'
-import Terms                                      from './components/Terms'
-import Home                                       from './components/Home'
-import ViewSinglePost                             from './components/ViewSinglePost'
-import FlashMessages                              from './components/FlashMessages'
 import StateContext                               from './StateContext'
 import DispatchContext                            from './DispatchContext'
-import Axios                                      from 'axios'
+
+// My Components
+import CreatePost     from './components/CreatePost'
+import Header         from './components/Header'
+import HomeGuest      from './components/HomeGuest'
+import Footer         from './components/Footer'
+import About          from './components/About'
+import Terms          from './components/Terms'
+import Home           from './components/Home'
+import ViewSinglePost from './components/ViewSinglePost'
+import FlashMessages  from './components/FlashMessages'
+import Profile        from './components/Profile'
+
+// Axios
+import Axios from 'axios'
 
 Axios.defaults.baseURL = 'http://localhost:3333'
 
 function Main () {
 
-    const initialState = {
-        loggedIn: Boolean(localStorage.getItem('appToken')),
-        flashMessages: [],
-        user: {
-            token: localStorage.getItem('appToken'),
-            username: localStorage.getItem('appUsername'),
-            avatar: localStorage.getItem('appAvatar')
-        }
-    }
+	const initialState = {
+		loggedIn: Boolean(localStorage.getItem('appToken')),
+		flashMessages: [],
+		user: {
+			token: localStorage.getItem('appToken'),
+			username: localStorage.getItem('appUsername'),
+			avatar: localStorage.getItem('appAvatar')
+		}
+	}
 
-    function ourReducer (draft, action) {
-        switch (action.type) {
-            case 'login':
-                draft.loggedIn = true
-                draft.user = action.data
-                return
-            case 'logout':
-                draft.loggedIn = false
-                return
-            case 'flashMessage':
-                draft.flashMessages.push(action.value)
-                return
-        }
-    }
+	function ourReducer (draft, action) {
+		switch (action.type) {
+			case 'login':
+				draft.loggedIn = true
+				draft.user = action.data
+				return
+			case 'logout':
+				draft.loggedIn = false
+				return
+			case 'flashMessage':
+				draft.flashMessages.push(action.value)
+				return
+		}
+	}
 
-    const [state, dispatch] = useImmerReducer(ourReducer, initialState)
+	const [state, dispatch] = useImmerReducer(ourReducer, initialState)
 
-    useEffect(() => {
-        if (state.loggedIn) {
-            localStorage.setItem('appToken', state.user.token)
-            localStorage.setItem('appUsername', state.user.username)
-            localStorage.setItem('appAvatar', state.user.avatar)
-        } else {
-            localStorage.removeItem('appToken')
-            localStorage.removeItem('appUsername')
-            localStorage.removeItem('appAvatar')
-        }
-    }, [state.loggedIn])
+	useEffect(() => {
+		if (state.loggedIn) {
+			localStorage.setItem('appToken', state.user.token)
+			localStorage.setItem('appUsername', state.user.username)
+			localStorage.setItem('appAvatar', state.user.avatar)
+		} else {
+			localStorage.removeItem('appToken')
+			localStorage.removeItem('appUsername')
+			localStorage.removeItem('appAvatar')
+		}
+	}, [state.loggedIn])
 
-    return (
-        <StateContext.Provider value={ state }>
-            <DispatchContext.Provider value={ dispatch }>
-                <BrowserRouter>
-                    <FlashMessages messages={ state.flashMessages } />
-                    <Header />
-                    <Switch>
-                        <Route path={ '/' }
-                               exact>
-                            { state.loggedIn ? <Home /> : <HomeGuest /> }
-                        </Route>
-                        <Route path={ '/about-us' }>
-                            <About />
-                        </Route>
-                        <Route path={ '/terms' }>
-                            <Terms />
-                        </Route>
-                         <Route path={ '/post/:id' }>
-                            <ViewSinglePost />
-                        </Route>
-                        <Route path={ '/create-post' }>
-                            <CreatePost />
-                        </Route>
-                    </Switch>
-                    <Footer />
-                </BrowserRouter>
-            </DispatchContext.Provider>
-        </StateContext.Provider>
-    )
+	return (
+		<StateContext.Provider value={ state }>
+			<DispatchContext.Provider value={ dispatch }>
+				<BrowserRouter>
+					<FlashMessages messages={ state.flashMessages } />
+					<Header />
+					<Switch>
+						<Route path={ '/' }
+							   exact>
+							{ state.loggedIn ? <Home /> : <HomeGuest /> }
+						</Route>
+						<Route path={ '/profile/:username' }>
+							<Profile />
+						</Route>
+						<Route path={ '/about-us' }>
+							<About />
+						</Route>
+						<Route path={ '/terms' }>
+							<Terms />
+						</Route>
+						<Route path={ '/post/:id' }>
+							<ViewSinglePost />
+						</Route>
+						<Route path={ '/create-post' }>
+							<CreatePost />
+						</Route>
+					</Switch>
+					<Footer />
+				</BrowserRouter>
+			</DispatchContext.Provider>
+		</StateContext.Provider>
+	)
 }
 
 ReactDOM.render(<Main />, document.querySelector('#app'))
 
 if (module.hot) {
-    module.hot.accept()
+	module.hot.accept()
 }
