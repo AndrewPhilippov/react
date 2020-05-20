b78f2e1dfbaca9c9ce13b02a8a578262e32e13b3
Andrew <a@fandb.pro>
2020-05-20 23:30:57 +0300

    Delete post functionality added

--- BRAD/COMPLEX-APP/app/components/ViewSinglePost.js
+++ BRAD/COMPLEX-APP/app/components/ViewSinglePost.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import ReactMarkdown                  from 'react-markdown'
-import Page                           from './Page'
-import LoadingDotsIcon                from './LoadingDotsIcon'
-import Axios                          from 'axios'
-import { useParams, Link }            from 'react-router-dom'
-import ReactTooltip                   from 'react-tooltip'
-import NotFound                       from './NotFound'
+import React, { useEffect, useState, useContext } from 'react'
+import ReactMarkdown                              from 'react-markdown'
+import Page                                       from './Page'
+import LoadingDotsIcon                            from './LoadingDotsIcon'
+import Axios                                      from 'axios'
+import { useParams, Link, withRouter }            from 'react-router-dom'
+import ReactTooltip                               from 'react-tooltip'
+import NotFound                                   from './NotFound'
+import StateContext                               from '../StateContext'
+import DispatchContext                            from '../DispatchContext'
 
-function ViewSinglePost () {
+function ViewSinglePost (props) {
+	const appState = useContext(StateContext)
+	const appDispatch = useContext(DispatchContext)
 	const ourRequest = Axios.CancelToken.source()
 	const { id } = useParams()
 	const [isLoading, setIsLoading] = useState(true)
@@ -39,23 +43,51 @@ function ViewSinglePost () {
 	const date = new Date(post.createdDate)
 	const dateFormatted = `${ date.getMonth() + 1 }/${ date.getDate() }/${ date.getFullYear() }`
 
+	function isOwner () {
+		if (appState.loggedIn) {
+			console.log('appState.user.username === post.author.username', appState.user.username === post.author.username)
+			return appState.user.username === post.author.username
+		}
+		return false
+	}
+
+	async function deleteHandler () {
+		const areYouSure = window.confirm('Do you really want to delete this post?')
+		if (areYouSure) {
+			try {
+				const response = await Axios.delete(`/post/${ id }`, { data: { token: appState.user.token } })
+				if (response.data === 'Success') {
+					//	1. Display a flash message
+					appDispatch({ type: 'flashMessage', value: 'Post was successfully deleted!' })
+					//	2. Redirect back to the current user's profile
+					props.history.push(`/profile/${ appState.user.username }`)
+				}
+			} catch (e) {
+				console.log('There was a problem')
+			}
+		}
+	}
+
 	return (
 		<Page title={ post.title }>
 			<div className="d-flex justify-content-between">
 				<h2>{ post.title }</h2>
-				<span className="pt-2">
+				{ isOwner() && (
+					<span className="pt-2">
                     <Link to={ `/post/${ post._id }/edit` }
 						  data-tip={ 'Edit' }
 						  data-for={ 'edit' }
 						  className="text-primary mr-2"><i className="fas fa-edit"></i></Link>{ ' ' }
-					<ReactTooltip id={ 'edit' }
-								  className={ 'custom-tooltip' } />
-                    <a className="delete-post-button text-danger"
+						<ReactTooltip id={ 'edit' }
+									  className={ 'custom-tooltip' } />
+                    <a onClick={ deleteHandler }
+					   className="delete-post-button text-danger"
 					   data-tip={ 'Delete' }
 					   data-for={ 'delete' }><i className="fas fa-trash"></i></a>
 						<ReactTooltip id={ 'delete' }
 									  className={ 'custom-tooltip' } />
                 </span>
+				) }
 			</div>
 
 			<p className="text-muted small mb-4">
@@ -79,4 +111,4 @@ function ViewSinglePost () {
 	)
 }
 
-export default ViewSinglePost
+export default withRouter(ViewSinglePost)
